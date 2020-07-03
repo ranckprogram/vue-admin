9a856f53032937c45cefdbd24c1f820f5a9b2bde
ranckprogram <1474574022@qq.com>
2020-07-03 18:12:00 +0800

    init

---
+++ src/utils/index.js
@@ -0,0 +1,18 @@
+export const add = () => {
+  let a = 1;
+  let b = 2;
+  return a + b;
+};
+
+export function getName() {
+  const firstName = '123';
+
+  const lastName = '333';
+  return [firstName, lastName];
+}
+
+export const parser = data => {
+  return data;
+};
+
+export const arr = [1, 23, 4, 51];
